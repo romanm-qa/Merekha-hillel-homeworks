72d9a550e53018c6d12e795d23cd1f09a928b2c7
Roman Merekha <romanmerekha@gmail.com>
2026-07-06 16:35:02 +0300

    Complete homework 13

---
+++ lesson_13_logging/homework_13.py
@@ -0,0 +1,41 @@
+"""
+Ваша команда та ви розробляєте систему входу для веб-додатка,
+і вам потрібно реалізувати тести на функцію для логування подій в системі входу.
+Дано функцію, напишіть набір тестів для неї.
+"""
+
+import logging
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / "login_system.log"
+
+def log_event(username: str, status: str):
+    """
+    Логує подію входу в систему.
+
+    username: Ім'я користувача, яке входить в систему.
+
+    status: Статус події входу:
+
+    * success - успішний, логується на рівні інфо
+    * expired - пароль застаріває і його слід замінити, логується на рівні warning
+    * failed  - пароль невірний, логується на рівні error
+    """
+    log_message = f"Login event - Username: {username}, Status: {status}"
+
+    # Створення та налаштування логера
+    logging.basicConfig(
+        filename=LOG_FILE,
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s',
+        force=True
+        )
+    logger = logging.getLogger("log_event")
+
+    # Логування події
+    if status == "success":
+        logger.info(log_message)
+    elif status == "expired":
+        logger.warning(log_message)
+    else:
+        logger.error(log_message)
